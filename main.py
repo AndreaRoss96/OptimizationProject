0a6cf86910f2d0964bda18bf7ab99c496ddffd43
filorazi <orazifilippo@gmail.com>
2020-04-04 17:11:21 +0200

    import dataset

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from neural_network import NeuralNetwork
+
 
 def main():
-    pass
+    ds = pd.read_csv("wine.data")
+    figure = plt.figure(figsize=(5,5))
+    print(ds.head())
+    Y = ds["Class"]
+    X = ds.drop("Class", axis = 1)
+    nn = NeuralNetwork()
+
 
 if __name__ == "__main__":
     main()
