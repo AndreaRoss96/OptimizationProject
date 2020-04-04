76dee8129bf1bb12244720efda8a689a68a8ad7a
AndreaRoss96 <andrea.rossolini2@studio.unibo.it>
2020-04-04 15:43:07 +0200

    function.py added

---
+++ functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+'''
+Sigmoid is used in binary classification problem [-1,1]
+ReLu is mainly used to perform threshold operation to each input elem where val <0 is set to 0 (better performance in generalization)
+Softmax is mainly used for multi-class problem
+'''
+#def sigmoid = lambda x: 1 / (1 + np.exp(-x))
+def sigmoid(x):
+    return (1/(1 + np.exp(-x)))
+
+def relu(x):
+    return np.maximum(0,x)
+
+def softmax(x):
+    exp = np.exp(x)
+    exp_sum = np.sum(np.exp(x))
+    return exp/exp_sum
