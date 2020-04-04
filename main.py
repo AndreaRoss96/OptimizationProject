4015ed966a2e8ea05402da87711a889e82bc247f
filorazi <orazifilippo@gmail.com>
2020-04-04 17:52:31 +0200

    gitignore, traintest split and utils

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import numpy as np
 from neural_network import NeuralNetwork
+from sklearn.model_selection import train_test_split
+import random
 
 
 def main():
     ds = pd.read_csv("wine.data")
-    figure = plt.figure(figsize=(5,5))
     print(ds.head())
+    seed = 42
+    #seed = random.randint(0,100)
     Y = ds["Class"]
     X = ds.drop("Class", axis = 1)
-    nn = NeuralNetwork()
+    i_train, i_test, o_train, o_test = train_test_split(
+            X, Y, train_size=0.33, random_state = seed)
+    #nn = NeuralNetwork()
 
 
 if __name__ == "__main__":
