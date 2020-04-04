7a781f6f1be2274119ac4bc80263f8da52ce0f33
filorazi <orazifilippo@gmail.com>
2020-04-04 17:23:55 +0200

    utils

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 from neural_network import NeuralNetwork
 
 
---
+++ utils.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Printer:
+    def __init__(self):
+        figure = plt.figure(figsize=(10,10))
+        pass
+
+    def plotDif(self, pred_o, true_o, feat_1, feat_2):
+        sns.scatterplot(feat_1, feat_2, style =list(pred_o==true_o), hue= list(true_o));
+
+        pass
