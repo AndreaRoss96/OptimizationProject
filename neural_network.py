ead5d861646fbb56620d77e7d7bdc661240f81a5
AndreaRoss96 <andrea.rossolini2@studio.unibo.it>
2020-04-05 11:33:57 +0200

    nn class i mprovements

--- neural_network.py
+++ neural_network.py
@@ -10,18 +10,29 @@ class NeuralNetwork:
         functions: activation function for each layer
 
         self parameters
-
+        input: input layer
+        weights: stores the weights between each layer
+        output, y: output layer
+        functions: activation function on each layer
+                    (if functions are less than layers, the functions list will be filled with the last function of the list)
+        z[i]: neuron value
         '''
         self.input      = layer_dimensions[0]
         self.y          = layer_dimensions[-1] # last layer of the array
         self.output     = np.zeros(self.y.shape)
-
+        self.functions  = functions
         self.weights    = []
-        for i in range(1, len(layer_dimensions)):
-            w.append(np.random.rand(layer_dimensions[i-1],layer_dimensions[i]))
 
+        for i in range(1, len(layer_dimensions)):
+            # random initialization of weights
+            self.weights.append(np.random.rand(layer_dimensions[i-1],layer_dimensions[i]))
+
+        for len(self.functions) < len(layer_dimensions)-1 :
+            # to fill the list of function, in case these are less than the number of layer
+            self.functions.append(self.functions[-1])
 
     def feedforward(self):
+        # # TODO:  salva lo stato dei nodi (z)
         self.layer1 = sigmoid(np.dot(self.input, self.weights1))
         self.output = sigmoid(np.dot(self.layer1, self.weights2))
 
