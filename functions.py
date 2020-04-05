3e5a411961786fb25d9ed950827d5970162b48a4
AndreaRoss96 <andrea.rossolini2@studio.unibo.it>
2020-04-05 20:15:06 +0200

    improovements in nn class, added function selectors and new cost functions

--- functions.py
+++ functions.py
@@ -9,13 +9,70 @@ def sigmoid(x): return (1/(1 + np.exp(-x)))
 
 def relu(x): return np.maximum(0,x)
 
+def hyperbolic(x): return np.tanh(x)
+
 def softmax(x):
+    # return an np.array
     exp = np.exp(x)
     exp_sum = np.sum(np.exp(x))
     return exp/exp_sum
 
-def sigmoid_derivative(x): return (sigmoid(x)*(1-sigmoid(x)))
 
-def reluderiv(x):
+
+'''
+v DERIVATIVE v
+'''
+def sigmoid_deriv(x): return (sigmoid(x)*(1-sigmoid(x)))
+
+def relu_deriv(x):
   if x >= 0: return 1
   else: return 0
+
+def hyperbolic_deriv(x): return (1/np.cosh(x))**2
+
+def softmax_deriv(x):
+    # example x[0] = i  x[-1] = l
+    #(e^i*sum(e^j)) / (sum(e^n))^2)   -- j:[i+1, l] and n:[i, l]
+    res = []
+    den = (np.sum(np.exp(x)))**2
+    for elem in x:
+        num = np.sum(np.exp(x))-np.exp(elem)
+        res.append((np.exp(elem)*num)/(den))
+    return res
+
+'''
+v COST FUNCTIONS v
+'''
+def mean_squared_error(x, y):
+    # works for np's arrays
+    return np.square(x - y).mean()
+
+def cross_entropy(m, a, Y):
+    # m: number of samples
+    # a: activation values in the output layer
+    # Y: true values
+    return -(1/m) * np.sum(Y*np.log(a) + (1-Y)*np.log(1-a))
+
+
+'''
+Functions selctors
+
+With the string passed as a value it is possible to get the desired function
+'''
+def activation(func):
+    options = {
+        "sigmoid": sigmoid,
+        "relu":relu,
+        "hyperbolic":hyperbolic,
+        "softmax":softmax
+    }
+    return options.get(func.lower())
+
+def derivative(func):
+    options = {
+        "sigmoid": sigmoid_deriv,
+        "relu":relu_deriv,
+        "hyperbolic":hyperbolic_deriv,
+        "softmax":softmax_deriv
+    }
+    return options.get(func.lower())
--- neural_network.py
+++ neural_network.py
@@ -14,7 +14,7 @@ class NeuralNetwork:
         input: input layer
         weights - array of arrays: stores the weights between each layer
         # output, y: output layer -- cancelled
-        functions: activation function on each layer
+        functions - list[String]: activation function on each layer
                     (if functions are less than layers, the functions list will be filled with the last function of the list)
         z - array of arrays: contains all the neuron of each hidden layer and output layer
         a - array of arrays: activation value at hidden layer
@@ -26,12 +26,11 @@ class NeuralNetwork:
         # self.output     = np.zeros(self.y.shape)
         self.functions  = functions
         self.weights, self.z = [], []
-        self.a          = [[np.zeros(layers[0])]] # initialization activation value of the input layer
         self.partial_deri = []
         self.gradient   = []
-
+        self.a          = [[np.zeros(layers[0])]] # initialization activation value of the input layer
         for i in range(1, self.n_layers):
-            # random initialization of weights
+            # initialization of parameters and random weights
             self.weights.append(np.random.rand(layers[i-1],layers[i]))
             self.z.append(np.zeros(self.layers[i]))
             self.a.append(np.zeros(self.layers[i]))
@@ -42,23 +41,24 @@ class NeuralNetwork:
             # to fill the list of function, in case these are less than the number of layer
             self.functions.append(self.functions[-1])
 
+
     def feedforward(self):
         for i in range(0, self.n_layers-1):
             self.z[i] = np.dot(self.a[i], self.weights[i])
-            vectFunc = np.vectorize(self.functions[i]) # vectorization of the layer's activation function
+            vectFunc = np.vectorize(activation(self.functions[i])) # vectorization of the layer's activation function
             self.a[i+1] = vectFunc(self.z[i])
 
     """fit NN with the training model_selection
     a batch size can be set to obtain a batch stocastic GD"""
     def fit(input_train, output_train, input_val, epoch, batch_size=0):
         #creating the gradient table
-        if batch_size is not 0:
-            batchs = [input_train[x:x+batch_size] for x in range(0, len(input_train), batch_size)]
+        if batch_size != 0:
+            batches = [input_train[x:x+batch_size] for x in range(0, len(input_train), batch_size)]
         else:
-            batchs = [input_train]
+            batches = [input_train]
         for i in range(1, self.n_layers):
             self.gradient.append(np.zeros(layers[i-1],layers[i]))
-        for batch in batchs:
+        for batch in batches:
             update_batch(batch,true_out)
 
 
